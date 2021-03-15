986178da7a59831307736a814dbd8e531b2f7703
lor-enz <git@lorenz.kiwi>
2021-03-15 21:13:53 +0100

    Add pandas data processing

--- code/covid.py
+++ code/covid.py
@@ -6,79 +6,172 @@ import time
 import datetime
 import sys
 import os
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import logging
 from io import BytesIO
 from socket import gaierror
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import Select
 
 import csv
 import requests
 import numpy as np
+import pandas as pd
+import pickle
 
-CSV_URL = 'https://raw.githubusercontent.com/ard-data/2020-rki-impf-archive/master/data/9_csv_v2/region_BY.csv'
+logging.basicConfig(level=logging.INFO)
 
-DAILY_VACC_TIME_IN_SECS = 36000
+# url, filename, keyname
+CSV_URL_VACC = (
+    'https://raw.githubusercontent.com/ard-data/2020-rki-impf-archive/master/data/9_csv_v2/region_BY.csv', 'vacc.csv', 'last_download_vacc')
+CSV_URL_INFE = (
+    "https://opendata.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0.csv", 'infe.csv', 'last_download_infe')
+
+
+DAILY_VACC_TIME_IN_SECS = 36000  # 36000 = 10 hours
 LOOK_BACK_FOR_MEAN = 7
+OLDNESS_THRESHOLD = 3600 * 12  # 3600 = 1 hour
+PICKLE_FILE = 'storage.p'
 
 
-class Checker():
+class Databook():
 
-    def __str__(self):
-        return f"website: {self.website} \nnumber: {self.some_number}"
+    storage = {
+        'last_download_vacc': 0,
+        'last_download_infe': 0
+    }
 
     def __init__(self):
-        self.download_data()
+        self.load_pickle()
+        self.load_dataframes()
 
-    def download_data(self):
-        with requests.Session() as s:
-            download = s.get(CSV_URL)
-            decoded_content = download.content.decode('utf-8')
-            cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-            self.all_rows = list(cr)
-            return self.all_rows
+    def load_pickle(self):
+        if (os.path.isfile(PICKLE_FILE)):
+            with open(PICKLE_FILE, 'br') as f:
+                self.storage = pickle.load(f)
+        logging.info(
+            f"Loaded Pickle... vacc {self.storage['last_download_vacc']} & infe {self.storage['last_download_infe']} ")
+
+    def save_pickle(self):
+        logging.info(
+            f"Saving Pickle... vacc {self.storage['last_download_vacc']} & infe {self.storage['last_download_infe']} ")
+
+        with open(PICKLE_FILE, 'bw') as f:
+            pickle.dump(self.storage, f)
+
+    def is_fresh_data_needed(self, what):
+        is_needed = False
+        filename = what[1]
+        keyname = what[2]
+        if not os.path.isfile(filename):
+            is_needed = True
+
+        oldness = ((time.time() - self.storage[keyname]) / 60)
+        now = datetime.datetime.now()
+        is_working_hours = now.hour > 6 and now.hour < 20
+
+        if (is_working_hours and oldness > OLDNESS_THRESHOLD):
+            is_needed = True
+
+        if is_needed:
+            logging.info(f"New {keyname} data is needed...")
+        return is_needed
+
+    def get_infe_last_update_timestamp(self, filename):
+        # TODO this should be done better, and with pandas not with csv, but pandas was being difficult...
+        rows = []
+        with open(filename, newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for row in reader:
+                rows.append(row)
+        date_string = rows[-1][34][3:13]
+        date_time_obj = datetime.datetime.strptime(
+            date_string, '%d.%m.%Y')
+
+        timestamp = int(date_time_obj.replace(
+            tzinfo=datetime.timezone.utc).timestamp())
+        return timestamp
+
+    def maybe_download_data(self, what):
+        if self.is_fresh_data_needed(what):
+            self.download_data(what)
+
+    def download_data(self, what):
+        keyname = what[2]
+        response = requests.get(what[0])
+        filename = what[1]
+
+        with open(filename, 'w') as f:
+            writer = csv.writer(f)
+            for line in response.iter_lines():
+                writer.writerow(line.decode('utf-8').split(','))
+        if (filename == 'vacc.csv'):
+            df = pd.read_csv(filename, sep=',',
+                             index_col=0, parse_dates=True)
+            last_update = ((df.tail(1).index.astype(
+                np.int64) // 10**9) + 60*60*(24+8)).tolist()[0]
+            self.storage[keyname] = last_update
+            self.save_pickle()
+        elif (filename == 'infe.csv'):
+            last_update = self.get_infe_last_update_timestamp(
+                filename)
+            self.storage[keyname] = last_update
+            self.save_pickle()
+        else:
+            logging.error(
+                f"Unknown filename! Expected vacc.csv or infe.csv. Got {filename}")
+
+    def load_dataframes(self):
+        self.maybe_download_data(CSV_URL_VACC)
+        self.maybe_download_data(CSV_URL_INFE)
+        self.load_vacc_dataframe()
+        self.load_infe_dataframe()
+
+    def load_infe_dataframe(self):
+        # INFE
+        csv = CSV_URL_INFE[1]
+        df = pd.read_csv(csv, sep=',')
+        df = df[['last_update', 'BL', 'GEN', 'county',
+                 'cases7_per_100k', 'cases7_bl_per_100k']]
+        self.df_infe = df
+
+    def load_vacc_dataframe(self):
+        # VACC
+        csv = CSV_URL_VACC[1]
+        df = pd.read_csv(
+            csv, sep=',', index_col=0, parse_dates=True)
+        df = df[['dosen_kumulativ', 'impf_inzidenz_dosen']]
+        df['dosen_kumulativ_differenz_zum_vortag'] = df.dosen_kumulativ - \
+            df.dosen_kumulativ.shift(1)
+
+        df['dosen_kumulativ_differenz_zum_vortag'] = df['dosen_kumulativ_differenz_zum_vortag'].fillna(
+            0)
+        df = df.astype({'dosen_kumulativ_differenz_zum_vortag': 'int64'})
+        self.df_vacc = df
 
     def get_current_abs_doses(self):
-        i = 1
-        last_row = []
-        while len(last_row) < 4:
-            last_row = self.all_rows[len(self.all_rows)-i]
-            i = i+1
-        vaccinated_abs = int(last_row[2])
-        date = last_row[0]
-        return {
-            "date": date,
-            "vaccinated_abs": vaccinated_abs,
-        }
+        return self.df_vacc.tail(1)['dosen_kumulativ'].values[0]
+
+    def get_data_date(self):
+        return self.df_vacc.tail(1).index.values[0]
 
     def get_extrapolated_abs_doses(self):
-        current_info = self.get_current_abs_doses()
+        official_doses = self.get_current_abs_doses()
         # current_info["vaccinated_abs"]
-        hm = current_info["date"]
-        print(f"newest data date {hm}")
-        info_unix = int(time.mktime(datetime.datetime.strptime(
-            current_info["date"], "%Y-%m-%d").timetuple()))
-        # info_unix is 0:00 of that day, but the data shows the vaccs at the END of that day.
-        # So add 24 hours to it. and then also 8 hours until it's 8am
-        info_unix = info_unix + 60*60*24 + 60*60*8
+        official_doses_timestamp = self.storage['last_download_vacc']
 
         current_time = int(time.time())
-        time_difference_secs = current_time - info_unix
+        time_difference_secs = current_time - official_doses_timestamp
         print(
-            f"time_difference_secs {time_difference_secs} current_time {current_time} info_unix {info_unix}")
+            f"time_difference_secs {time_difference_secs} current_time {current_time} official_doses_timestamp {official_doses_timestamp}")
         # Let's say vaccinations happen from 8:00 to 18:00 Uhr so 10 hours or 36000 secs
+
+        time_difference_secs = min(time_difference_secs,
+                                   DAILY_VACC_TIME_IN_SECS)
         mean = self.get_average_daily_vaccs_of_last_days(LOOK_BACK_FOR_MEAN)
         todays_vaccs = self.extrapolate(mean, time_difference_secs)
-        vaccs_til_8am = int(current_info["vaccinated_abs"])
-        total_vaccs = vaccs_til_8am + todays_vaccs
+        total_vaccs = official_doses + todays_vaccs
         print(f"""Using mean {mean} of last {LOOK_BACK_FOR_MEAN} days 
         to calculate todays newest vaccs estimate based on {time_difference_secs} seconds (or {time_difference_secs / 60 / 60} hours)
         since 8am. Resulting in todays vaccs til now being {todays_vaccs}
-        Adding that to offical vaccs of {vaccs_til_8am}
+        Adding that to offical vaccs of {official_doses}
         results in total vaccs of {total_vaccs}""")
         return total_vaccs
 
@@ -89,20 +182,11 @@ class Checker():
         return todays_vaccs
 
     def get_average_daily_vaccs_of_last_days(self, days_to_look_back):
-        daylies = []
-        previous = 0
-        skip = True
-        for row in self.all_rows:
-            if skip:
-                skip = False
-                continue
-            daylies.append(int(row[2])-previous)
-            previous = int(row[2])
-        daylies = daylies[-days_to_look_back:]
-        mean = int(np.mean(daylies))
+        mean = self.df_vacc.tail(days_to_look_back)[
+            'dosen_kumulativ_differenz_zum_vortag'].values.mean()
         print(f"Mean of last {days_to_look_back} days is: {mean}")
         return mean
 
-    def print_all(self):
-        for row in self.all_rows:
-            print(row)
+
+databook = Databook()
+print(f"get_current_abs_doses   {databook.get_current_abs_doses()}")
--- code/test_covid.py
+++ code/test_covid.py
@@ -3,29 +3,34 @@ import unittest
 
 class TestChecker(unittest.TestCase):
 
+    def test_get_current_abs_doses(self):
+        from covid import Databook
+        databook = Databook()
+        doses = databook.get_current_abs_doses()
+        self.assertGreater(doses, 1500000)
+
     def test_get_extrapolated_abs_doses(self):
-        from covid import Checker
-        checker = Checker()
-        total_vaccs = checker.get_extrapolated_abs_doses()
+        from covid import Databook
+        databook = Databook()
+        total_vaccs = databook.get_extrapolated_abs_doses()
         print(f"total_vaccs {total_vaccs}")
-        self.assertGreater(total_vaccs, int(checker.get_current_abs_doses()[
-                           "vaccinated_abs"]))
+        self.assertGreater(total_vaccs, int(databook.get_current_abs_doses()))
 
     def test_get_average_daily_vaccs_of_last_days(self):
-        from covid import Checker
-        checker = Checker()
-        mean = checker.get_average_daily_vaccs_of_last_days(7)
+        from covid import Databook
+        databook = Databook()
+        mean = databook.get_average_daily_vaccs_of_last_days(7)
         print(f"mean: {mean}")
         self.assertGreater(mean, 30000)
         self.assertLess(mean, 70000)
 
     def test_extrapolate(self):
-        from covid import Checker
-        checker = Checker()
+        from covid import Databook
+        databook = Databook()
 
-        self.assertEqual(checker.extrapolate(50000, 360000), 50000)
-        self.assertEqual(checker.extrapolate(50000, 36000), 50000)
-        self.assertEqual(checker.extrapolate(50000, 18000), 25000)
+        self.assertEqual(databook.extrapolate(50000, 360000), 50000)
+        self.assertEqual(databook.extrapolate(50000, 36000), 50000)
+        self.assertEqual(databook.extrapolate(50000, 18000), 25000)
 
 
 if __name__ == '__main__':
