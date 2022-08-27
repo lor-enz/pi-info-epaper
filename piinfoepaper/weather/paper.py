59fa7b5189cd08091bb9e425162c15b7c0c7c79f
Lorenz Burghardt <git@lorenz.kiwi>
2022-08-27 12:27:15 +0200

    Restructure paper.py (in weather)

--- piinfoepaper/weather/paper.py
+++ piinfoepaper/weather/paper.py
@@ -30,10 +30,12 @@ def flip_partial(partial, paper_w, paper_h):
         new_y_s = paper_h - partial[3]
         return new_x_s, new_y_s, new_x_e, new_y_e
 
+
 class Alignment(Enum):
     TOP_LEFT = 0,
     TOP_RIGHT = 1,
     CENTERED = 2
+
 
 class Fill(Enum):
     GRAY1 = 1,
@@ -41,29 +43,49 @@ class Fill(Enum):
     GRAY3 = 3,
     GRAY4 = 4,
 
-layout = {
+
+layout_landscape = {
     # x- position, y-position, alignment, fontsize
     'time': (480, 0, Alignment.TOP_RIGHT, 12),
     'weekday': (125, 55, Alignment.CENTERED, 80),
     'day_date': (125, 125, Alignment.CENTERED, 45),
     'temp_min': (110, 210, Alignment.CENTERED, 80),
-    'temp_max': (480-110, 210, Alignment.CENTERED, 80),
+    'temp_max': (480 - 110, 210, Alignment.CENTERED, 80),
     'dash': (240, 210, Alignment.CENTERED, 80),
-    'icon1': (405-128, 90, Alignment.CENTERED, 0),
+    'icon1': (405 - 128, 90, Alignment.CENTERED, 0),
     'icon2': (405, 90, Alignment.CENTERED, 0),
     'one_icon': (341, 90, Alignment.CENTERED, 0),
 }
+
+layout_portrait = {
+    # x- position, y-position, alignment, fontsize
+    'time': (0, 0, Alignment.TOP_RIGHT, 12),
+    'weekday': (0, 0, Alignment.CENTERED, 80),
+    'day_date': (0, 0, Alignment.CENTERED, 45),
+    'temp_min': (0, 0, Alignment.CENTERED, 80),
+    'temp_max': (0, 0, Alignment.CENTERED, 80),
+    'dash': (0, 0, Alignment.CENTERED, 80),
+    'icon1': (0, 0, Alignment.CENTERED, 0),
+    'icon2': (0, 0, Alignment.CENTERED, 0),
+    'one_icon': (0, 0, Alignment.CENTERED, 0),
+}
+
+layout = layout_landscape
+
 
 class Paper:
 
     def __str__(self):
         return f"This is an object of the Paper class."
 
-    def __init__(self, databook: Databook, flip=False):
+    def __init__(self, databook: Databook, flip=False, debug=False):
         logging.debug(f'Init Paper at {mytime.current_time_hr()}')
         self.epd = epd3in7.EPD()
         self.flip = flip
+        self.debug = debug
         self.databook = databook
+        self.paper_image = Image.new('L', (self.epd.height, self.epd.width), 0xFF)
+        self.draw = ImageDraw.Draw(self.paper_image)
 
     def cancel_file_exists(self):
         if os.path.isfile("/home/pi/partial.cancel") or os.path.isfile("~/partial.cancel"):
@@ -71,7 +93,7 @@ class Paper:
             return True
         return False
 
-    def draw_weather_icon(self, paper_image, icon_nr, lay):
+    def draw_weather_icon(self, icon_nr, lay):
         if 10 <= icon_nr <= 31:
             img_file = f'weather/{icon_nr}.bmp'
         elif 1 <= icon_nr <= 9:
@@ -79,22 +101,24 @@ class Paper:
         else:
             img_file = f'weather/00.bmp'
 
-        self.draw_image(paper_image, img_file, lay)
+        self.draw_image(img_file, lay)
 
-    def draw_image(self, paper_image, img_filename, lay):
+    def draw_image(self, img_filename, lay):
         icon = Image.open(os.path.join(picdir, img_filename))
         width, height = icon.size
         x, y = self.calc_pos_from_alignment(lay, width, height)
-        paper_image.paste(icon, (x, y))
+        self.paper_image.paste(icon, (x, y))
 
-    def draw_text(self, draw, text, lay, fill=Fill.GRAY4):
+    def draw_text(self, text, lay, fill=Fill.GRAY4):
         font = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), lay[3])
         (width, height) = font.getsize(text)
         x, y = self.calc_pos_from_alignment(lay, width, height)
 
         logging.info(f"Drawing size {lay[3]} \'{text}\' at ({x}, {y})")
         # Label
-        draw.text((x, y), f'{text}', font=font, fill=self.fill2grey(fill))
+        self.draw.text((x, y), f'{text}', font=font, fill=self.fill2grey(fill))
+        if self.debug:
+            self.draw.rectangle((20, 50, 70, 100), outline=0)
 
     def calc_pos_from_alignment(self, lay, width, height):
         if lay[2] == Alignment.TOP_LEFT:
@@ -109,38 +133,38 @@ class Paper:
         return x, y
 
     def draw_data(self):
-        epd = self.epd
-        epd.init(0)
-        epd.Clear(0xFF, 0)
+
+        self.epd.init(0)
+        self.epd.Clear(0xFF, 0)
 
         try:
-            paper_image = Image.new('L', (epd.height, epd.width), 0xFF)
-            draw = ImageDraw.Draw(paper_image)
+            self.paper_image = Image.new('L', (self.epd.height, self.epd.width), 0xFF)
+            self.draw = ImageDraw.Draw(self.paper_image)
             # Current time - small in the corner
-            self.draw_text(draw, f'{mytime.current_time_hr("%d %b %H:%M")}', layout['time'])
+            self.draw_text(f'{mytime.current_time_hr("%d %b %H:%M")}', layout['time'])
             # # # # # # # # # # # # #
             # DAY
-            self.draw_text(draw, self.databook.get_day_of_week(), layout['weekday'])
-            self.draw_text(draw, self.databook.get_pretty_date(), layout['day_date'])
+            self.draw_text(self.databook.get_day_of_week(), layout['weekday'])
+            self.draw_text(self.databook.get_pretty_date(), layout['day_date'])
 
             # ICONS
             if self.databook.are_icons_different():
-                self.draw_weather_icon(paper_image, self.databook.get_icon1(), layout['icon1'])
-                self.draw_weather_icon(paper_image, self.databook.get_icon2(), layout['icon2'])
+                self.draw_weather_icon(self.databook.get_icon1(), layout['icon1'])
+                self.draw_weather_icon(self.databook.get_icon2(), layout['icon2'])
             else:
-                self.draw_weather_icon(paper_image, self.databook.get_icon1(), layout['one_icon'])
+                self.draw_weather_icon(self.databook.get_icon1(), layout['one_icon'])
             # TEMP
-            self.draw_text(draw, self.databook.get_temp_min(), layout['temp_min'] )
-            self.draw_text(draw, "-", layout['dash'], fill=Fill.GRAY3 )
-            self.draw_text(draw, self.databook.get_temp_max(), layout['temp_max'])
+            self.draw_text(self.databook.get_temp_min(), layout['temp_min'])
+            self.draw_text("-", layout['dash'], fill=Fill.GRAY3)
+            self.draw_text(self.databook.get_temp_max(), layout['temp_max'])
 
             # # # # # # # # # # # # #
             # save as file, maybe flip, then push to display
-            paper_image.save(r'image.png')
+            self.paper_image.save(r'image.png')
             if self.flip:
-                paper_image = paper_image.transpose(Image.ROTATE_180)
-            epd.display_4Gray(epd.getbuffer_4Gray(paper_image))
-            epd.sleep()
+                self.paper_image = self.paper_image.transpose(Image.ROTATE_180)
+            self.epd.display_4Gray(self.epd.getbuffer_4Gray(self.paper_image))
+            self.epd.sleep()
         except IOError as e:
             logging.info(e)
 
@@ -167,7 +191,6 @@ class Paper:
         if (freshness.value == 2):
             return self.epd.GRAY2
         return self.epd.GRAY1
-
 
     def clear(self):
         self.epd.init(0)
