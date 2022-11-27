50dd95454dcc44a516a7bb81327e8cad0985edd6
Lorenz Burghardt <git@lorenz.kiwi>
2022-11-27 22:50:51 +0100

    🐛 Fix import error in startup.py

--- piinfoepaper/startup.py
+++ piinfoepaper/startup.py
@@ -8,11 +8,11 @@ from paper_layout import PaperLayout
 from paper_elements import PaperTextElement
 from paper_enums import Orientation, Alignment, Fill
 from welcome.welcome import Welcome
-from welcome.technical_info import TechnicalInfo
 
 # supposed to run after plugging in the device. Shows welcome message and technical info.
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     logging.info(f'## Start Welcome display at {mytime.current_time_hr()}')
+    # layout = Welcome(15).get_plugmein_layout()
     layout = Welcome(15).get_layout()
     paper_controller = PaperController(layout)
